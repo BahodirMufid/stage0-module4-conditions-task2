c170b543dcaeb22a9a3b01400c568d3526e87a50
bahod <bahodirmufid@gmail.com>
2022-10-21 22:24:06 +0500

    all files up to date and checked correctly

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -1,7 +1,16 @@
 package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
-    public void throwCoin(int from1UpTo1000) {
 
+    public static void main(String[] args) {
+        throwCoin(1000);
+    }
+    public static void throwCoin(int from1UpTo1000) {
+
+        if (from1UpTo1000<=499){
+            System.out.print("Eagle\n");
+        } else if (from1UpTo1000>=500) {
+            System.out.print("Tail\n");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
+    public static void main(String[] args) {
+        printGreatest(-400,1);
+    }
+    public static void printGreatest(int first, int second) {
+
+        if (first>second){
+            System.out.println(first);
+        }else if (first<second){
+            System.out.println(second);
+        }
     }
 }
